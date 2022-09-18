e57f3067fc53defe6e20827d743b2957de3f29a9
g4dalcom <g4dalcom@gmail.com>
2022-09-18 21:52:08 +0900

    Comment Entity genertate

---
+++ src/main/java/com/dolphin/demo/domain/Comment.java
@@ -0,0 +1,44 @@
+package com.dolphin.demo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.util.Lazy;
+import javax.persistence.*;
+
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+public class Comment extends Timestamped {
+
+    @Id
+    @Column(name = "comment_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(nullable = false)
+    private String title;
+
+
+    @Column(nullable = false)
+    private String content;
+
+
+    private String imageUrl;
+
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "place_id")
+//    private Place place;
+//
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "member_id")
+//    private Member member;
+
+}
