148322a400bde2a6418e622b7fe8b2c97ac08d31
yonykk <97495661+yonykk@users.noreply.github.com>
2022-09-22 18:22:00 +0900

    무중단 배포 쉘 추가

---
+++ scripts/run_new_was.sh
@@ -0,0 +1,32 @@
+# run_new_was.sh
+
+#!/bin/bash
+
+PROJECT_ROOT="/home/ubuntu/app"
+
+
+CURRENT_PORT=$(cat /etc/nginx/conf.d/service-url.inc| grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=0
+
+echo "> Current port of running WAS is ${CURRENT_PORT}."
+
+if [ ${CURRENT_PORT} -eq 8081 ]; then
+  TARGET_PORT=8082
+elif [ ${CURRENT_PORT} -eq 8082 ]; then
+  TARGET_PORT=8081
+else
+  echo "> No WAS is connected to nginx"
+fi
+
+TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
+
+if [ ! -z ${TARGET_PID} ]; then
+  echo "> Kill WAS running at ${TARGET_PORT}."
+  sudo kill ${TARGET_PID}
+fi
+
+echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
+cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
+nohup java -jar -Dserver.port=${TARGET_PORT} $PROJECT_ROOT/build/libs/* > /home/app/nohup.out 2>&1 &
+echo "> Now new WAS runs at ${TARGET_PORT}."
+exit 0
