c29f21980cf373c73f92894f6eca32b9b37982c2
g4dalcom <g4dalcom@gmail.com>
2022-09-24 18:09:53 +0900

    comment dto 코드 정리

--- src/main/java/com/dolphin/demo/dto/response/CommentResponseDto.java
+++ src/main/java/com/dolphin/demo/dto/response/CommentResponseDto.java
@@ -1,14 +1,10 @@
 package com.dolphin.demo.dto.response;
 
-import com.dolphin.demo.domain.Comment;
-import com.dolphin.demo.domain.Image;
-import com.dolphin.demo.dto.request.ImageRequestDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -30,16 +26,5 @@ public class CommentResponseDto {
     private int star;
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
-
-    public CommentResponseDto(Comment comment, List<ImageResponseDto> imageList) {
-        this.comment_id = comment.getId();
-        this.place_id = comment.getPlace().getId();
-        this.title = comment.getTitle();
-        this.content = comment.getContent();
-        this.imageList = imageList;
-        this.star = comment.getStar();
-        this.createdAt = comment.getCreatedAt();
-        this.modifiedAt = comment.getModifiedAt();
-    }
 
 }
