b9d02a05aaa92a86c1a2743dbc98c498a20798e2
prscsl <prscsl@naver.com>
2022-10-22 01:06:55 +0900

    월드컵 선출반환 메서드 추가

--- src/main/java/com/dolphin/demo/controller/WorldCupController.java
+++ src/main/java/com/dolphin/demo/controller/WorldCupController.java
@@ -1,10 +1,14 @@
 package com.dolphin.demo.controller;
 
+import com.dolphin.demo.dto.request.WorldCupRequestDto;
 import com.dolphin.demo.dto.response.PlaceListResponseDto;
+import com.dolphin.demo.dto.response.WorldCupResponseDto;
 import com.dolphin.demo.service.WorldCupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,7 +20,12 @@ public class WorldCupController {
     private final WorldCupService worldCupService;
 
     @GetMapping(value = "/api/place/worldcup")
-    public ResponseEntity<List<List<PlaceListResponseDto>>> makeWorldCup(){
+    public ResponseEntity<List<List<WorldCupResponseDto>>> makeWorldCup(){
         return worldCupService.makeWorldCup();
+    }
+
+    @PostMapping(value = "/api/place/reworldcup")
+    public ResponseEntity<List<List<WorldCupResponseDto>>> reWorldCup(@RequestBody WorldCupRequestDto worldCupRequestDto){
+        return worldCupService.reWorldCup(worldCupRequestDto);
     }
 }
