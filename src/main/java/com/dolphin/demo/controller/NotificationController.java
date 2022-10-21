0cb0cffd397073bd2a4e037e66f98572ac3a691d
prscsl <110075438+prscsl@users.noreply.github.com>
2022-10-21 22:39:59 +0900

    알림 읽음 상태 변경
    알림 읽음 상태 변경

--- src/main/java/com/dolphin/demo/controller/NotificationController.java
+++ src/main/java/com/dolphin/demo/controller/NotificationController.java
@@ -39,7 +39,7 @@ public class NotificationController {
      * 알림 읽음 상태 변경
      */
     @PatchMapping("/api/auth/notice/read")
-    public ResponseEntity<Void> readNotification(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public ResponseEntity<Void> readNotification(@PathVariable Long id) {
         notificationService.readNotification(userDetails.getMember().getId());
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
