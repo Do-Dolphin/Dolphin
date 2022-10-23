87f39df9f51aa463d3ee071b0f4ee694fae3ab32
prscsl <prscsl@naver.com>
2022-10-23 20:08:08 +0900

    이상형 월드컵 필터(전체지역, 세부지역, 테마) 추가

--- src/main/java/com/dolphin/demo/controller/WorldCupController.java
+++ src/main/java/com/dolphin/demo/controller/WorldCupController.java
@@ -1,15 +1,11 @@
 package com.dolphin.demo.controller;
 
 import com.dolphin.demo.dto.request.WorldCupRequestDto;
-import com.dolphin.demo.dto.response.PlaceListResponseDto;
 import com.dolphin.demo.dto.response.WorldCupResponseDto;
 import com.dolphin.demo.service.WorldCupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,8 +16,10 @@ public class WorldCupController {
     private final WorldCupService worldCupService;
 
     @GetMapping(value = "/api/place/worldcup")
-    public ResponseEntity<List<List<WorldCupResponseDto>>> makeWorldCup(){
-        return worldCupService.makeWorldCup();
+    public ResponseEntity<List<List<WorldCupResponseDto>>> makeWorldCup(@RequestParam(value = "areaCode") String areaCode,
+                                                                        @RequestParam(value = "sigunguCode") String sigunguCode,
+                                                                        @RequestParam(value = "themes") String themes){
+        return worldCupService.makeWorldCup(areaCode, sigunguCode, themes);
     }
 
     @PostMapping(value = "/api/place/reworldcup")
--- src/main/java/com/dolphin/demo/service/WorldCupService.java
+++ src/main/java/com/dolphin/demo/service/WorldCupService.java
@@ -4,6 +4,8 @@ import com.dolphin.demo.domain.Place;
 import com.dolphin.demo.dto.request.WorldCupRequestDto;
 import com.dolphin.demo.dto.response.PlaceListResponseDto;
 import com.dolphin.demo.dto.response.WorldCupResponseDto;
+import com.dolphin.demo.exception.CustomException;
+import com.dolphin.demo.exception.ErrorCode;
 import com.dolphin.demo.repository.PlaceImageRepository;
 import com.dolphin.demo.repository.PlaceRepository;
 import com.google.common.collect.Lists;
@@ -23,8 +25,38 @@ public class WorldCupService {
     private final PlaceImageRepository placeImageRepository;
 
 
-    public ResponseEntity<List<List<WorldCupResponseDto>>> makeWorldCup() {
-        List<Place> placeList = placeRepository.findAll();
+    public ResponseEntity<List<List<WorldCupResponseDto>>> makeWorldCup(String areaCode, String sigunguCode, String themes) {
+        List<Place> placeList;
+        if(areaCode.equals("0")){
+            if(themes.equals("0")){
+                placeList = placeRepository.findAll();
+            }else {
+                placeList = placeRepository.findAllByTheme(themes);
+            }
+            if(placeList.size() < 32){
+                throw new CustomException(ErrorCode.FAIL_FIND_AREA);
+            }
+        }else if(sigunguCode.equals("0")){
+            if(themes.equals("0")){
+                placeList = placeRepository.findAllByAreaCode(areaCode);
+            }else {
+                placeList = placeRepository.findAllByAreaCodeAndTheme(areaCode, themes);
+            }
+            if(placeList.size() < 32){
+                throw new CustomException(ErrorCode.FAIL_FIND_AREA);
+            }
+        }else if(themes.equals("0")){
+            placeList = placeRepository.findAllByAreaCodeAndSigunguCode(areaCode, sigunguCode);
+            if(placeList.size() < 32){
+                throw new CustomException(ErrorCode.FAIL_FIND_AREA);
+            }
+        }else {
+            placeList = placeRepository.findAllByAreaCodeAndSigunguCodeAndTheme(areaCode, sigunguCode, themes);
+            if(placeList.size() < 32){
+                throw new CustomException(ErrorCode.FAIL_FIND_AREA);
+            }
+        }
+
         List<WorldCupResponseDto> placeListResponseDtoList = new ArrayList<>();
         for (int i = 0; i < 32; i++) {
             int index = (int) (Math.random() * placeList.size());
@@ -36,12 +68,14 @@ public class WorldCupService {
                         .image(placeImageRepository.findFirstByPlace(placeList.get(index)).get().getImageUrl())
                         .build();
                 placeListResponseDtoList.add(placeListResponseDto);
+                placeList.remove(index);
             } else{
                 WorldCupResponseDto placeListResponseDto = WorldCupResponseDto.builder()
                         .id(placeList.get(index).getId())
                         .title(placeList.get(index).getTitle())
                         .build();
                 placeListResponseDtoList.add(placeListResponseDto);
+                placeList.remove(index);
             }
         }
         List<List<WorldCupResponseDto>> partition = Lists.partition(placeListResponseDtoList, 2);
