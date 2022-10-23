1b5e13c9cc83b28b00706919d90b0a0f8c2061ff
prscsl <110075438+prscsl@users.noreply.github.com>
2022-10-23 20:31:19 +0900

    find 조건 추가
    find 조건 추가

--- src/main/java/com/dolphin/demo/repository/PlaceRepository.java
+++ src/main/java/com/dolphin/demo/repository/PlaceRepository.java
@@ -7,12 +7,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PlaceRepository extends JpaRepository<Place, Long>, PlaceRepositoryCustom {
+
+    List<Place> findAllByAreaCode(String areaCode);
     List<Place> findAllByAreaCodeAndSigunguCodeAndTheme(String areaCode, String sigunguCode, String theme, PageRequest pageRequest);
     boolean existsByAreaCodeAndSigunguCode(String areaCode, String sigunguCode);
+    List<Place> findAllByAreaCodeAndSigunguCode(String areaCode, String sigunguCode);
     List<Place> findAllByAreaCodeAndSigunguCodeAndTheme(String areaCode, String sigunguCode, String theme);
     List<Place> findAllByAreaCodeAndTheme(String areaCode, String theme);
     List<Place> findAllByAreaCodeAndTheme(String areaCode, String theme, PageRequest pageRequest);
     List<Place> findAllByTheme(String theme, PageRequest pageRequest);
+    List<Place> findAllByTheme(String theme);
     List<Place> findAllByThemeOrderByReadCountDesc(String theme, PageRequest pageRequest);
     Place findTopByOrderByIdDesc();
     List<Place> findAllByContent(String content, PageRequest pageRequest);
