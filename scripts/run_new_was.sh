5ef76702df2018bde7a3105bf2fe15c72b83335f
yonykk <gwecs626@gmail.com>
2022-10-26 10:28:27 +0900

    java heap space 크기 증가

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -26,6 +26,6 @@ if [ ! -z ${TARGET_PID} ]; then
 fi
 
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
-nohup java -jar -Dserver.port=${TARGET_PORT} $PROJECT_ROOT/spring-webapp.jar > /home/ubuntu/app/nohup.out 2>&1 &
+nohup java -Xms256m -Xmx512m -jar -Dserver.port=${TARGET_PORT} $PROJECT_ROOT/spring-webapp.jar > /home/ubuntu/app/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
